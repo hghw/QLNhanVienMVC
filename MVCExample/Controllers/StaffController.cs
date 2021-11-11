eaccee742ae8c10f3003b9f0590079cb4fc795a1
Hoàng Gia Huy <89172440+hghw@users.noreply.github.com>
2021-11-11 11:55:30 +0700

    11/11 upfile table html css

---
+++ MVCExample/Controllers/StaffController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCSamples.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCExample.Controllers
+{
+    public class StaffController : Controller
+
+    {
+        private readonly ILogger<StaffController> _logger;
+
+        public StaffController(ILogger<StaffController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Console.WriteLine("Đang xây dựng");
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            //var test = HttpContext.Session.GetObjectFromJson<List<string>>("Test");
+            Console.WriteLine("Đang xây dựng");
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            //var test = HttpContext.Session.GetObjectFromJson<List<string>>("Test");
+            Console.WriteLine("Đang xây dựng");
+            return View();
+        }
+        public IActionResult Update()
+        {
+            //var test = HttpContext.Session.GetObjectFromJson<List<string>>("Test");
+            Console.WriteLine("Đang xây dựng");
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            //var test = HttpContext.Session.GetObjectFromJson<List<string>>("Test");
+            Console.WriteLine("Đang xây dựng");
+            return View();
+        }
+        public IActionResult Report()
+        {
+            //var test = HttpContext.Session.GetObjectFromJson<List<string>>("Test");
+            Console.WriteLine("Đang xây dựng");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- MVCExample/Models/NhanVien.cs
+++ MVCExample/Models/NhanVien.cs
@@ -7,5 +7,11 @@ namespace MVCExample.Models
 {
     public class NhanVien
     {
+        public string maNhanVien { set ; get ;}
+        public string hoTen { set; get; }
+        public string ngaySinh { set; get; }
+        public string sdt { set; get; }
+        public string diaChi { set; get; }
+        public string soNamCongTac { set; get; }
     }
 }
